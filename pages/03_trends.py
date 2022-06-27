6690611723abb461804743ff8df13e7b41b7a9d1
Yoonsen <Yoonsen@gmail.com>
2022-06-27 11:31:17 +0200

    ed

--- pages/03_trends.py
+++ pages/03_trends.py
@@ -17,7 +17,7 @@ st.image(image, width = 200)
 st.markdown('Les om [Digital Humaniora - DH](https://nb.no/dh-lab) ved Nasjonalbiblioteket')
 
 
-st.title('Søk i norsk-amerikanske aviser')
+st.title('Fordeling av ord over år i norsk-amerikanske aviser')
 
 corpus = make_crps()
 search = st.text_input('Finn trender for ord, enkeltord skilt med komma', "", help="Kommaseparert liste med ord, ingen trunkering")
@@ -26,6 +26,7 @@ searchlist = [x.strip() for x in search.split(',')]
 if not search == "":
     trends = dh.Counts(corpus = corpus, words=searchlist)
     df = pd.merge(trends.counts.transpose(), corpus.corpus.set_index('urn')[["year"]], left_index=True, right_index=True)
+    df["year"] = pd.to_datetime(df.year, format="%Y")
     #st.dataframe(trends.counts.transpose())
     #st.dataframe(df)
     st.bar_chart(df.groupby('year').sum())
